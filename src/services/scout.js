35d70ec0b5d5eddec4fd7b49e096beeff2919998
Hamil <dimapanat.hamil@gmail.com>
2023-06-16 17:30:18 -0400

    json server link update

--- src/services/scout.js
+++ src/services/scout.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/scoutingReports";
+const baseUrl = "https://nba-page-cf0253c037d1.herokuapp.com/scoutingReports";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
